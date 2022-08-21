33640cfbef1761a923bd189a99ed3194d55d02d4
Dolly Singh <dollyrajpoot07@gmail.com>
2022-08-21 21:19:51 +0530

    Initial Commit

---
+++ PrintIthNode.java
@@ -0,0 +1,108 @@
+﻿// Print ith Node
+// For a given a singly linked list of integers and a position 'i', print the node data at the 'i-th' position.
+// Note : Assume that the Indexing for the singly linked list always starts from 0. If the given position 'i',  is greater than the length of the given singly linked list, then don't print anything.
+// Input format :
+// The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow. The first line of each test case or query contains the elements of the singly linked list separated by a single space. The second line contains the value of 'i'. It denotes the position in the given singly linked list.
+// Remember/Consider :
+// While specifying the list elements for input, -1 indicates the end of the singly linked list and hence, would never be a list element.
+// Output format :
+// For each test case, print the node data at the 'i-th' position of the linked list(if exists). Output for every test case will be printed in a seperate line.
+// Constraints :
+// 1 <= t <= 10^2
+// 0 <= N <= 10^5
+// i  >= 0
+// Time Limit: 1sec
+// Sample Input 1 :
+// 1
+// 3 4 5 2 6 1 9 -1
+// 3
+// Sample Output 1 :
+// 2
+// Sample Input 2 :
+// 2
+// 3 4 5 2 6 1 9 -1
+// 0
+// 9 8 4 0 7 8 -1
+// 3
+// Sample Output 2 :
+// 3
+// 0
+// CODE:
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+
+    public LinkedListNode(T data) {
+        this.data = data;
+    }
+}
+
+class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static LinkedListNode<Integer> takeInput() throws IOException {
+        LinkedListNode<Integer> head = null, tail = null;
+
+        String[] datas = br.readLine().trim().split("\\s");
+
+        int i = 0;
+        while (i < datas.length && !datas[i].equals("-1")) {
+            int data = Integer.parseInt(datas[i]);
+            LinkedListNode<Integer> newNode = new LinkedListNode<Integer>(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            i += 1;
+        }
+
+        return head;
+    }
+
+    public static void print(LinkedListNode<Integer> head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t > 0) {
+
+            LinkedListNode<Integer> head = takeInput();
+            int i = Integer.parseInt(br.readLine().trim());
+            PrintIthNode.printIthNode(head, i);
+
+            t -= 1;
+        }
+    }
+}
+
+public class PrintIthNode {
+    public static void printIthNode(LinkedListNode<Integer> head, int i) {
+        LinkedListNode<Integer> temporary = head;
+        int count = 0;
+        if (temporary == null) {
+            return;
+        }
+        while (temporary != null) {
+            if (count == i) {
+                System.out.println(temporary.data);
+            }
+            count++;
+            temporary = temporary.next;
+        }
+    }
+}
